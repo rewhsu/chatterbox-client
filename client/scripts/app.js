94010548ccc43e0e33df0ae19bf5658d1b650e20
rewhsu <andrewhsu42@gmail.com>
2017-01-02 15:17:03 -0800

    Implement send method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,23 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};

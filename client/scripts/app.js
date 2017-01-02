9cf781e6a3863570d7386155dc395a273bfe96c0
rewhsu <andrewhsu42@gmail.com>
2017-01-02 15:18:25 -0800

    Implement fetch method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -21,3 +21,19 @@ app.send = function(message) {
     }
   });
 };
+
+app.fetch = function(requestUrl) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: requestUrl,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message fetched');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to fetch message', data);
+    }
+  });
+};
